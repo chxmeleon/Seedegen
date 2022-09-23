37fdad3835d9d1c487cac46f7c45117b8fb8ee0b
ash <chxmeleonweng@gmail.com>
2022-09-23 17:21:54 +0800

    [BUILD] build next js project
    install tailwindcss sass husky lint-staged

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="w-full h-screen m-auto flex justify-center items-center">
+      <h1 className="font-semibold text-7xl">Hi, there</h1>
+    </div>
+  )
+}
+
+export default Home
