194f241a5edb996333b51b8cfabd97bff0d7bf41
ash <chxmeleonweng@gmail.com>
2022-10-17 10:49:48 +0800

    [BUGFIX] fix 500 api error

--- src/components/elements/ProfitLeaderboard.tsx
+++ src/components/elements/ProfitLeaderboard.tsx
@@ -3,56 +3,53 @@ import useSWR from 'swr'
 const fetcher = (url: RequestInfo | URL) => fetch(url).then((r) => r.json())
 
 const ProfitLeaderboard = () => {
-  const url = 'api/data/gain-rank-30days'
-  const ethPriceUrl =
-    'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD'
+  // const url = 'api/data/gain-rank-30days'
+  // const ethPriceUrl =
+  //   'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD'
 
-  const { data: tradeData } = useSWR(url, fetcher)
-  const { data: ethToUsd } = useSWR(ethPriceUrl, fetcher)
+  // const { data: tradeData } = useSWR(url, fetcher)
+  // const { data: ethToUsd } = useSWR(ethPriceUrl, fetcher)
 
-  console.log(tradeData)
-  console.log(ethToUsd)
-  
-  const requestMaxValue = () => {
-    const keyName = ['profit', 'received', 'spent', 'roi']
-    const newKeyArr = [...new Array(4)].map((_val, idx) => {
-      const key = keyName[idx]
-      const requestKeyArr = tradeData.map((data: any) => {
-        return data[key]
-      })
-      return requestKeyArr.sort((a: number, b: number) => a - b).reverse()[0]
-    })
-    return newKeyArr
-  }
-
-  const rankContent = [...new Array(10)].map((_val, idx) => {
-    const allData = tradeData?.[idx]
-    const spent = allData?.spent.toFixed(2)
-    const revernced = allData?.received.toFixed(2)
-    const profit = allData?.profit.toFixed(2)
-    const roi = allData?.roi.toFixed(2)
-    const maxProfit = requestMaxValue()[0]
-    const maxRevernced = requestMaxValue()[1]
-    const maxRoi = requestMaxValue()[2]
-    const maxSpent = requestMaxValue()[3]
-
-    return (
-      <ProfitRank
-        key={idx.toString()}
-        id={idx.toString()}
-        ens={allData?.ens}
-        address={allData?.address}
-        spent={spent}
-        maxSpent={maxSpent}
-        revernced={revernced}
-        maxRevernced={maxRevernced}
-        profit={profit}
-        maxProfit={maxProfit}
-        roi={roi}
-        maxRoi={maxRoi}
-      />
-    )
-  })
+  // const requestMaxValue = () => {
+  //   const keyName = ['profit', 'received', 'spent', 'roi']
+  //   const newKeyArr = [...new Array(4)].map((_val, idx) => {
+  //     const key = keyName[idx]
+  //     const requestKeyArr = tradeData.map((data: any) => {
+  //       return data[key]
+  //     })
+  //     return requestKeyArr.sort((a: number, b: number) => a - b).reverse()[0]
+  //   })
+  //   return newKeyArr
+  // }
+  //
+  // const rankContent = [...new Array(10)].map((_val, idx) => {
+  //   const allData = tradeData?.[idx]
+  //   const spent = allData?.spent.toFixed(2)
+  //   const revernced = allData?.received.toFixed(2)
+  //   const profit = allData?.profit.toFixed(2)
+  //   const roi = allData?.roi.toFixed(2)
+  //   const maxProfit = requestMaxValue()[0]
+  //   const maxRevernced = requestMaxValue()[1]
+  //   const maxRoi = requestMaxValue()[2]
+  //   const maxSpent = requestMaxValue()[3]
+  //
+  //   return (
+  //     <ProfitRank
+  //       key={idx.toString()}
+  //       id={idx.toString()}
+  //       ens={allData?.ens}
+  //       address={allData?.address}
+  //       spent={spent}
+  //       maxSpent={maxSpent}
+  //       revernced={revernced}
+  //       maxRevernced={maxRevernced}
+  //       profit={profit}
+  //       maxProfit={maxProfit}
+  //       roi={roi}
+  //       maxRoi={maxRoi}
+  //     />
+  //   )
+  // })
 
   return (
     <>
@@ -71,7 +68,7 @@ const ProfitLeaderboard = () => {
             <div>Spent</div>
             <div>ROI</div>
           </div>
-          <ul className='rank-content'>{rankContent}</ul>
+          {/* <ul className='rank-content'>{rankContent}</ul> */}
         </div>
       </section>
     </>
