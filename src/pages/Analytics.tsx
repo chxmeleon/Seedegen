5c2b2904cd639a5193e444a40dca28edc183293c
ash <chxmeleonweng@gmail.com>
2022-10-18 14:55:22 +0800

    [FEATURE] test serversideprops with prisma

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from 'next'
-import ProfitLeaderboard from '../components/elements/ProfitLeaderboard'
+import ProfitLeaderboard30D from '../components/elements/ProfitLeaderboard30D'
 import Layout from '../components/layouts/Layout'
 
 const Analytics: NextPage = () => {
   return (
     <>
       <Layout>
-        <div className="w-full m-auto pt-32 pb-4 flex justify-center items-center">
-          <ProfitLeaderboard />
+        <div className="w-full m-auto pt-32 pb-4 px-12 flex justify-center items-center">
+          <ProfitLeaderboard30D />
         </div>
       </Layout>
     </>
