d029ee4bb61d1a4a58249a5ebbb2bb2a32b54d2e
ash <chxmeleonweng@gmail.com>
2022-11-07 21:00:32 +0800

    [BUGFIX] set swr to false to fix chartjs bugs

--- src/pages/address/[id].tsx
+++ src/pages/address/[id].tsx
@@ -22,7 +22,7 @@ const Trader = (props: any) => {
   const totalSpent = rankData.spent.toFixed(2)
   const totalReceived = rankData.received.toFixed(2)
   const totalRoi = rankData.roi.toFixed(2)
-  const avatarText = ens.slice(0, 1).toUpperCase()  
+  const avatarText = ens.slice(0, 1).toUpperCase()
   const address = winLoseData?.[0]?.walletAddress.toString()
   const shortAddress = address.slice(0, 4) + '...' + address.slice(-4)
   const statusArray = winLoseData.map((item: any) => item.status.toLowerCase())
@@ -39,7 +39,7 @@ const Trader = (props: any) => {
     setPage(v)
   }
 
-  const currentPage = winLoseData?.slice(0 + offset, 10 + offset)
+  const currentPage = winLoseData?.slice(offset, 10 + offset)
 
   const winLoseBoard = currentPage.map((data: any, idx: any) => {
     const status = data?.status.toLowerCase()
@@ -59,198 +59,196 @@ const Trader = (props: any) => {
     const project = data?.projectAddress
 
     const projectName = () => {
-      const a = collections.find((x: any) => x.address === project)
-      return a?.project ?? project.slice(0, 4) + '...' + project.slice(-4) 
+      const all = collections.find((x: any) => x.address === project)
+      return all?.project ?? project.slice(0, 4) + '...' + project.slice(-4)
     }
-    console.log(project);
-    
 
-return (
-  <WinAndLoseForm
-    key={idx.toString()}
-    id={'winlose-' + idx.toString()}
-    spent={spent}
-    revernced={revernced}
-    status={transMode()}
-    profit={profit}
-    spentTxHashUrl={buyTxHashUrl}
-    recerncedTxHashUrl={sellTxHashUrl}
-    project={projectName()}
-    tokenId={tokenId}
-  />
-)
+    return (
+      <WinAndLoseForm
+        key={idx.toString()}
+        id={'winlose-' + idx.toString()}
+        spent={spent}
+        revernced={revernced}
+        status={transMode()}
+        profit={profit}
+        spentTxHashUrl={buyTxHashUrl}
+        recerncedTxHashUrl={sellTxHashUrl}
+        project={projectName()}
+        tokenId={tokenId}
+      />
+    )
   })
 
-const pieOpt = {
-  plugins: {
-    legend: { display: false },
-  },
-}
-
-const pieData = {
-  labels: ['Lose', 'Win'],
-  datasets: [
-    {
-      data: [loseCounts, winCounts],
-      backgroundColor: ['#3b82f6', '#fbbf24'],
-      hoverBackgroundColor: ['#3b82f6', '#fbbf24'],
-      borderWidth: 0,
+  const pieOpt = {
+    plugins: {
+      legend: { display: false },
     },
-  ],
-}
+  }
 
-const barOpt = {
-  plugins: {
-    legend: { display: false },
-  },
-}
+  const pieData = {
+    labels: ['Lose', 'Win'],
+    datasets: [
+      {
+        data: [loseCounts, winCounts],
+        backgroundColor: ['#3b82f6', '#fbbf24'],
+        hoverBackgroundColor: ['#3b82f6', '#fbbf24'],
+        borderWidth: 0,
+      },
+    ],
+  }
 
-const barData = {
-  labels: ['Total Profit', 'Total Spent', 'Total Received', 'Gas Spent'],
-  datasets: [
-    {
-      data: [totalProfit, totalSpent, totalReceived, totalRoi * 50],
-      borderColor: ['#22d3ee'],
-      backgroundColor: ['#22d3ee'],
-      borderWidth: 1,
-      barThickness: 40,
+  const barOpt = {
+    plugins: {
+      legend: { display: false },
     },
-  ],
-}
+  }
 
-const DashContainer = tw.div`w-full h-full dark:bg-gray-900 flex justify-center text-center items-center rounded-md`
+  const barData = {
+    labels: ['Total Profit', 'Total Spent', 'Total Received', 'Gas Spent'],
+    datasets: [
+      {
+        data: [totalProfit, totalSpent, totalReceived, totalRoi * 50],
+        borderColor: ['#22d3ee'],
+        backgroundColor: ['#22d3ee'],
+        borderWidth: 1,
+        barThickness: 40,
+      },
+    ],
+  }
 
-return (
-  <Layout>
-    <div className="flex w-full">
-      <div className="py-24 px-16 mx-auto w-full max-w-[1300px]">
-        <div className="flex justify-between items-center py-5">
-          <div className="flex justify-between items-center mr-4 rounded-lg h-[250px] dark:bg-slate-800">
-            <div className="px-12">
-              <div className="flex justify-between">
-                <div className="flex overflow-hidden w-20 h-20 bg-amber-50 rounded-full">
-                  <span className="m-auto text-5xl font-bold text-zinc-600">
-                    {avatarText}
-                  </span>
+  const DashContainer = tw.div`w-full h-full dark:bg-gray-900 flex justify-center text-center items-center rounded-md`
+
+  return (
+    <Layout>
+      <div className="flex w-full">
+        <div className="py-24 px-16 mx-auto w-full max-w-[1300px]">
+          <div className="flex justify-between items-center py-5">
+            <div className="flex justify-between items-center mr-4 rounded-lg h-[250px] dark:bg-slate-800">
+              <div className="px-12">
+                <div className="flex justify-between">
+                  <div className="flex overflow-hidden w-20 h-20 bg-amber-50 rounded-full">
+                    <span className="m-auto text-5xl font-bold text-zinc-600">
+                      {avatarText}
+                    </span>
+                  </div>
+                  <div>
+                    <Link href={openseaUrl}>
+                      <a
+                        aria-label="trader opensea link"
+                        target="_blank"
+                        rel="noopener"
+                      >
+                        <div>opensea</div>
+                      </a>
+                    </Link>
+                    <Link href={etherscanUrl}>
+                      <a
+                        aria-label="trader etherscan link"
+                        target="_blank"
+                        rel="noopener"
+                      >
+                        <div>etherscan</div>
+                      </a>
+                    </Link>
+                  </div>
                 </div>
+                <div className="w-96">
+                  <h1 className="pt-5 pb-1 w-1/2 text-3xl font-medium truncate">
+                    {shortAddress}
+                  </h1>
+                  <div className="">{ens}</div>
+                </div>
+              </div>
+            </div>
+            <div className="mini-dashboard-grid">
+              <DashContainer>
                 <div>
-                  <Link href={openseaUrl}>
-                    <a
-                      aria-label="trader opensea link"
-                      target="_blank"
-                      rel="noopener"
-                    >
-                      <div>opensea</div>
-                    </a>
-                  </Link>
-                  <Link href={etherscanUrl}>
-                    <a
-                      aria-label="trader etherscan link"
-                      target="_blank"
-                      rel="noopener"
-                    >
-                      <div>etherscan</div>
-                    </a>
-                  </Link>
+                  <p>Total Profit</p>
+                  <p>{totalProfit}</p>
                 </div>
+              </DashContainer>
+              <DashContainer>
+                <div>
+                  <p>Total Spent</p>
+                  <p>{totalSpent}</p>
+                </div>
+              </DashContainer>
+              <DashContainer>
+                <div>
+                  <p>Total Revenue</p>
+                  <p>{totalReceived}</p>
+                </div>
+              </DashContainer>
+              <DashContainer>
+                <div>
+                  <p>Win & Lose</p>
+                  <p>
+                    {winCounts} / {loseCounts}
+                  </p>
+                </div>
+              </DashContainer>
+              <DashContainer>
+                <div>
+                  <p>ROI</p>
+                  <p>{totalRoi}</p>
+                </div>
+              </DashContainer>
+              <DashContainer>
+                <div>
+                  <p>Gas Spent</p>
+                  <p>{totalRoi * 50} ETH</p>
+                </div>
+              </DashContainer>
+            </div>
+          </div>
+          <div className="flex justify-between items-center pb-6 w-full">
+            <div className="flex flex-col items-center p-2 py-2.5 mr-2 w-2/3 rounded-md h-[425px] dark:bg-slate-800">
+              <div className="py-4 text-3xl font-semibold dark:text-slate-100">
+                Data Bar Chart
               </div>
-              <div className="w-96">
-                <h1 className="pt-5 pb-1 w-1/2 text-3xl font-medium truncate">
-                  {shortAddress}
-                </h1>
-                <div className="">{ens}</div>
+              <div className="p-4 w-full">
+                <Bar data={barData} options={barOpt} />
+              </div>
+            </div>
+            <div className="flex flex-col justify-evenly items-center p-5 ml-2 w-1/2 rounded-md h-[425px] dark:bg-slate-800">
+              <div className="pb-8 text-3xl font-semibold dark:text-slate-100">
+                Win & Lose
+              </div>
+              <div className="m-1 w-[230px]">
+                <Doughnut data={pieData} options={pieOpt} />
               </div>
             </div>
           </div>
-          <div className="mini-dashboard-grid">
-            <DashContainer>
-              <div>
-                <p>Total Profit</p>
-                <p>{totalProfit}</p>
-              </div>
-            </DashContainer>
-            <DashContainer>
-              <div>
-                <p>Total Spent</p>
-                <p>{totalSpent}</p>
-              </div>
-            </DashContainer>
-            <DashContainer>
-              <div>
-                <p>Total Revenue</p>
-                <p>{totalReceived}</p>
-              </div>
-            </DashContainer>
-            <DashContainer>
-              <div>
-                <p>Win & Lose</p>
-                <p>
-                  {winCounts} / {loseCounts}
-                </p>
-              </div>
-            </DashContainer>
-            <DashContainer>
-              <div>
-                <p>ROI</p>
-                <p>{totalRoi}</p>
-              </div>
-            </DashContainer>
-            <DashContainer>
-              <div>
-                <p>Gas Spent</p>
-                <p>{totalRoi * 50} ETH</p>
-              </div>
-            </DashContainer>
-          </div>
-        </div>
-        <div className="flex justify-between items-center pb-6 w-full">
-          <div className="flex flex-col items-center p-2 py-2.5 mr-2 w-2/3 rounded-md h-[425px] dark:bg-slate-800">
-            <div className="py-4 text-3xl font-semibold dark:text-slate-100">
-              Data Bar Chart
-            </div>
-            <div className="p-4 w-full">
-              <Bar data={barData} options={barOpt} />
-            </div>
-          </div>
-          <div className="flex flex-col justify-evenly items-center p-5 ml-2 w-1/2 rounded-md h-[425px] dark:bg-slate-800">
-            <div className="pb-8 text-3xl font-semibold dark:text-slate-100">
-              Win & Lose
-            </div>
-            <div className="m-1 w-[230px]">
-              <Doughnut data={pieData} options={pieOpt} />
-            </div>
-          </div>
-        </div>
 
-        <div className="py-14 pb-3 bg-gray-800 rounded-md">
-          <div className="grid grid-cols-7 gap-4 justify-items-center items-center py-2 pr-10 dark:bg-gray-700">
-            <div className="justify-self-start pl-12">Win/Lose</div>
-            <div className="">Project</div>
-            <div className="">tokenID</div>
-            <div className="">Profit(ETH)</div>
-            <div className="">Spent(ETH)</div>
-            <div className="">Revernced(ETH)</div>
-            <div className="">ROI(ETH)</div>
-          </div>
-          <div className="overflow-y-scroll h-[200px] win-lose-data-content">
-            {winLoseBoard}
-          </div>
-          <div className="flex justify-end px-3 pt-3 w-full dark:text-white">
-            <Stack spacing={2}>
-              <Pagination
-                count={counts}
-                size="large"
-                page={page}
-                onChange={handleChange}
-                className="pagination"
-              />
-            </Stack>
+          <div className="py-14 pb-3 bg-gray-800 rounded-md">
+            <div className="grid grid-cols-7 gap-4 justify-items-center items-center py-2 pr-10 dark:bg-gray-700">
+              <div className="justify-self-start pl-12">Win/Lose</div>
+              <div className="">Project</div>
+              <div className="">tokenID</div>
+              <div className="">Profit(ETH)</div>
+              <div className="">Spent(ETH)</div>
+              <div className="">Revernced(ETH)</div>
+              <div className="">ROI(ETH)</div>
+            </div>
+            <div className="overflow-y-scroll h-[200px] win-lose-data-content">
+              {winLoseBoard}
+            </div>
+            <div className="flex justify-end px-3 pt-3 w-full dark:text-white">
+              <Stack spacing={2}>
+                <Pagination
+                  count={counts}
+                  size="large"
+                  page={page}
+                  onChange={handleChange}
+                  className="pagination"
+                />
+              </Stack>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
