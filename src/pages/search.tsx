a9aa096a76b618464b9326ab5eedfbbe7ce6d215
ash <chxmeleonweng@gmail.com>
2022-11-29 18:33:57 +0800

    [FEATURE] add the search not found page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -23,11 +23,13 @@ const Search = () => {
   const apiPath = `api/data/search-data?q=${query}&page=${pageIndex}`
   const { data: searchData, error } = useSWR(apiPath, fetcher)
   const isLoading = !searchData && !error
+  const isNotFound = !searchData?.length 
 
   const hasNextPage = useMemo(
     () => pageIndex !== 10 && !!query,
     [pageIndex, query]
   )
+
   const loadMore = () => {
     setPageIndex(() => pageIndex + 1)
     setTimeout(() => {
@@ -46,7 +48,7 @@ const Search = () => {
   return (
     <>
       <Head>
-        <title>{router.query?.q?.slice(0,9)} - ICU Search</title>
+        <title>{router.query?.q?.slice(0, 9)} - ICU Search</title>
       </Head>
       <Layout>
         <section className="w-full">
@@ -54,16 +56,22 @@ const Search = () => {
             <div className="pb-10 m-auto w-full">
               <section className="flex items-center w-full">
                 <div className="m-auto w-full max-w-[1300px]">
-                  <div className="w-[45%] ml-[20%]">
-                    <SearchResult results={result} />
-                    {(isLoading || hasNextPage) && (
-                      <div ref={searchRef}>
-                        {[...new Array(3)].map((_val, idx: number) => (
-                          <SearchResultSkeleton key={idx} />
-                        ))}
-                      </div>
-                    )}
-                  </div>
+                  {isNotFound ? (
+                    <div className="mt-[28%]  flex justify-center items-center text-5xl font-medium">
+                      <h1>Search is Not Found</h1>
+                    </div>
+                  ) : (
+                    <div className="w-[45%] ml-[20%]">
+                      <SearchResult results={result} />
+                      {(isLoading || hasNextPage) && (
+                        <div ref={searchRef}>
+                          {[...new Array(3)].map((_val, idx: number) => (
+                            <SearchResultSkeleton key={idx} />
+                          ))}
+                        </div>
+                      )}
+                    </div>
+                  )}
                 </div>
               </section>
             </div>
